7fa7423d8be092d6c127c483fb3128b6c733266a
Masshiro <kwanwaimasshiro@gmail.com>
2025-01-11 19:28:35 -0800

    update

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+echo "=> Building AlphaRTC"
+
+output=$(gn --version 2>&1)
+if [[ $output =~ ^[0-9]{3,} ]]; then
+    echo "=> Checking gn... It is correctly configured: $output"
+else
+    echo "=> Checking gn... Required to configure gn, please wait..."
+    gclient sync && mv src/* .
+fi
+
+output_dir="out/Default"
+target_dir="target"
+target_lib_dir="${target_dir}/lib"
+target_bin_dir="${target_dir}/bin"
+target_pylib_dir="${target_dir}/pylib"
+
+if [ -d "${output_dir}" ]; then
+    echo "=> Directory ${output_dir} exists. Deleting..."
+    rm -rf "${output_dir}"
+fi
+
+if [ -d "${target_dir}" ]; then
+    echo "=> Directory ${target_dir} exists. Deleting..."
+    rm -rf "${target_dir}"
+fi
+
+
+echo "=> Building peerconnection_serverless using ninja..."
+gn gen "${output_dir}"
+ninja -C "${output_dir}" peerconnection_serverless
+
+
+echo "=> Creating target directories..."
+mkdir -p "${target_lib_dir}"
+mkdir -p "${target_bin_dir}"
+mkdir -p "${target_pylib_dir}"
+
+
+echo "=> Copying files and shared libraries..."
+cp -r examples/peerconnection/serverless/corpus/* "${target_bin_dir}"
+cp modules/third_party/onnxinfer/lib/*.so "${target_lib_dir}"
+cp modules/third_party/onnxinfer/lib/*.so.* "${target_lib_dir}"
+
+
+echo "=> Copying executables and Python scripts..."
+cp "${output_dir}/peerconnection_serverless" "${target_bin_dir}/peerconnection_serverless.origin"
+cp examples/peerconnection/serverless/peerconnection_serverless "${target_bin_dir}"
+cp modules/third_party/cmdinfer/*.py "${target_pylib_dir}/"
+
+
+echo "=> AlphaRTC build completed."
